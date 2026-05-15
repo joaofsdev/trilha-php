d9e856d1a037ced1f21d9bee2f457ebcf4c29bdd
joaofsdev <joao.franciscos047@gmail.com>
2026-05-15 09:43:51 -0300

    feat: adiciona exercicio 8 da lista

--- Exercicios/exercicio8.php
+++ Exercicios/exercicio8.php
@@ -3,3 +3,27 @@
 array, resulte na soma 6: 
 Não deve ser utilizado loops para resolver esse exercício. 
 array(1, 8, 13, 11, 1, 3, -2, -6, 5) */
+
+function encontrarElemento($array, $soma)
+{
+    if (empty($array)) {
+        return null;
+    }
+
+    $elemento = array_shift($array);
+
+    if (in_array($soma - $elemento, $array)) {
+        return $elemento;
+    }
+
+    return encontrarElemento($array, $soma);
+}
+
+$array = array(1, 8, 13, 11, 1, 3, -2, -6, 5);
+$resultado = encontrarElemento($array, 6);
+
+if ($resultado !== null) {
+    echo "O elemento encontrado é: " . $resultado;
+} else {
+    echo "Nenhum elemento encontrado que satisfaça a condição.";
+}
